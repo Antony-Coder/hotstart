80333ec5d96223040fdac9835d76ec6428efc9f5
Antony Pronchenko <nezlobtosha@gmail.com>
2020-07-08 01:31:00 +0300

    Story 182: fix bug

--- Scripts/Main.cs
+++ Scripts/Main.cs
@@ -2,12 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class main : MonoBehaviour
+public class Main : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("wow!");
     }
 
     // Update is called once per frame
